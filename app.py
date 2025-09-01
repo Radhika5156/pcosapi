bdb1cc349a8eaa7254f6bf07f23701516247545d
Radhika5156 <radhikavyas515@gmail.com>
2025-09-01 12:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ def predict():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
+        
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
